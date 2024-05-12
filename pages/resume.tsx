39a8857519d33bc2a21f2ad50ee7cd03c4ba218a
bilal jamil <bilal110d@gmail.com>
2024-05-12 15:40:34 +0500

    cahnge

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -20,56 +20,59 @@ const Resume = () => {
           </div>
         </div>
         <div>
-          <h5 className="my-3 text-2xl font-bold">Experience</h5>
-          <div className="">
-            <h5 className="my-2 text-xl font-bold">UI/UX Designer and 3D Artist.</h5>
-            <p className="font-semibold">Avanza Innovation</p>
-           
-          </div>
-          <div className="">
-            <h5 className="my-2 text-xl font-bold">Graphic Designer.</h5>
-            <p className="font-semibold"> IBEX Global</p>
-           
-          </div>
-          <div className="">
-            <h5 className="my-2 text-xl font-bold">Media Manager.</h5>
-            <p className="font-semibold">Cherry House of Fashion</p>
-           
-          </div>
-          <div className="">
-            <h5 className="my-2 text-xl font-bold">Project Coordinator.</h5>
-            <p className="font-semibold">Vital Tech Intl (Samsung/ Dahua CCTV)</p>
-           
-          </div>
-          <div className="">
-            <h5 className="my-2 text-xl font-bold">Mubarak Caterers and Decorators.</h5>
-            <p className="font-semibold">Project Coordinator</p>
-           
-          </div>
-          <div className="">
-            <h5 className="my-2 text-xl font-bold">DGR (Distinguish Gentlemen Ride).</h5>
-            <p className="font-semibold">Photographer</p>
-           
-          </div>
-          <div className="">
-            <h5 className="my-2 text-xl font-bold">Ride Pakistan.</h5>
-            <p className="font-semibold">Photographer</p>
-           
-          </div>
+
+        <div>
+        <h5 className="my-3 text-2xl font-bold">Tools & Softwares</h5>
+        <div className="my-2">
+          {tools.map((tool, i) => (
+            <Bar value={tool} key={i} />
+          ))}
+        </div>
+      </div>
+
+          
         </div>
       </div>
 
       {/*Languages & Tools */}
       <div className="grid gap-9 md:grid-cols-2">
-      <div>
-      <h5 className="my-3 text-2xl font-bold">Tools & Softwares</h5>
-      <div className="my-2">
-        {tools.map((tool, i) => (
-          <Bar value={tool} key={i} />
-        ))}
+     
+      <h5 className="my-3 text-2xl font-bold">Experience</h5>
+      <div className="">
+        <h5 className="my-2 text-xl font-bold">UI/UX Designer and 3D Artist.</h5>
+        <p className="font-semibold">Avanza Innovation</p>
+       
       </div>
-    </div>
-
+      <div className="">
+        <h5 className="my-2 text-xl font-bold">Graphic Designer.</h5>
+        <p className="font-semibold"> IBEX Global</p>
+       
+      </div>
+      <div className="">
+        <h5 className="my-2 text-xl font-bold">Media Manager.</h5>
+        <p className="font-semibold">Cherry House of Fashion</p>
+       
+      </div>
+      <div className="">
+        <h5 className="my-2 text-xl font-bold">Project Coordinator.</h5>
+        <p className="font-semibold">Vital Tech Intl (Samsung/ Dahua CCTV)</p>
+       
+      </div>
+      <div className="">
+        <h5 className="my-2 text-xl font-bold">Mubarak Caterers and Decorators.</h5>
+        <p className="font-semibold">Project Coordinator</p>
+       
+      </div>
+      <div className="">
+        <h5 className="my-2 text-xl font-bold">DGR (Distinguish Gentlemen Ride).</h5>
+        <p className="font-semibold">Photographer</p>
+       
+      </div>
+      <div className="">
+        <h5 className="my-2 text-xl font-bold">Ride Pakistan.</h5>
+        <p className="font-semibold">Photographer</p>
+       
+      </div>
       </div>
     </div>
   );
